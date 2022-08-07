d7cb31de1080280659ab7349a308c06ec196ccac
Andres Eduardo Garcia Marquez <andresgarcia0313@gmail.com>
2022-08-07 16:38:27 -0500

    Primera Versión

---
+++ execute.ps1
@@ -0,0 +1,6 @@
+docker build -t andresgarcia0313/portainer:latest .
+docker run -it --rm --name gestorcontenedores andresgarcia0313/portainer
+docker scan andresgarcia0313/portainer:latest
+docker-compose down
+docker-compose build
+docker-compose up -d
