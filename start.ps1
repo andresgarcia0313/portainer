1db0e3b4911b657cbf5721ffaae8a1ede3838cca
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2025-01-26 12:07:54 -0500

    cambio del nombre del proyecto en archivos de configuración y eliminación de comandos innecesarios en scripts de inicio

--- start.ps1
+++ start.ps1
@@ -1,7 +1,3 @@
-#docker login
-docker build -t andresgarcia0313/portainer:latest .
-#subir la imagen a dockerhub
-docker push andresgarcia0313/portainer:latest
 docker compose down
 docker compose build
 docker compose up -d
--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-#docker login
-docker build -t andresgarcia0313/portainer:latest .
-#subir la imagen a dockerhub
-docker push andresgarcia0313/portainer:latest
 docker compose down
 docker compose build
 docker compose up -d
